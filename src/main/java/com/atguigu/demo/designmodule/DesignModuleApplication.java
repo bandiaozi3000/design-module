be373fc32eee6ad58f1da9f076f3b35dd18e3a1b
bandiaozi3000 <slmzed7537@163.com>
2020-02-20 15:22:46 +0800

    设计模式

---
+++ src/main/java/com/atguigu/demo/designmodule/DesignModuleApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.demo.designmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/demo/designmodule/DesignModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.demo.designmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
